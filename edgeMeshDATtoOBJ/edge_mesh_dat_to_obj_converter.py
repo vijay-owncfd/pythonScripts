e367f871bfbc42bf3294fa01033815b1ed63da57
vijay-owncfd <vijay.owncfd@gmail.com>
2023-12-27 09:37:55 +0530

    Added a tutorial case

--- edgeMeshDATtoOBJ/edge_mesh_dat_to_obj_converter.py
+++ edgeMeshDATtoOBJ/edge_mesh_dat_to_obj_converter.py
@@ -23,8 +23,8 @@ def read_dat_file(file_path):
         dat_line_end = []      # Ending vertex of the edges in the .dat file
           
         # Read the next numPoints lines of the .dat file and populate the dat_vertex, coordinate values
-		# After numPoints number of lines, read the next numEdges lines containing edge (line) information and populate the 
-		# starting and ending vertices in dat_line_start and dat_line_end arrays
+        # After numPoints number of lines, read the next numEdges lines containing edge (line) information and populate the 
+        # starting and ending vertices in dat_line_start and dat_line_end arrays
         for i in range(numPoints+numEdges):
             line = file.readline().strip().split()
             if(i<numPoints):
@@ -68,7 +68,7 @@ def create_obj_file(file_path, numPoints, dat_vertex, coordX, coordY, coordZ, nu
         for i in range(numPoints):
             dat_obj_dict[dat_vertex[i]] = obj_vertex[i]
         
-		# Writing the edges (l)
+        # Writing the edges (l)
         for i in range(numEdges):
             obj_file.write(f"l {dat_obj_dict[dat_line_start[i]]} {dat_obj_dict[dat_line_end[i]]}\n")
             
@@ -77,10 +77,10 @@ def create_obj_file(file_path, numPoints, dat_vertex, coordX, coordY, coordZ, nu
 # Loop over all the .dat files
 numFiles = len(sys.argv)
 
-for i in range(1, numFiles):	
+for i in range(1, numFiles):    
     file_path = sys.argv[i]
     numPoints, dat_vertex, coordX, coordY, coordZ, numEdges, dat_line_start, dat_line_end = read_dat_file(file_path)
-	
+    
     # Create the .obj file
     obj_file_path = create_obj_file(file_path, numPoints, dat_vertex, coordX, coordY, coordZ, numEdges, dat_line_start, dat_line_end)
     print(f"The .obj file has been created: {obj_file_path}")
